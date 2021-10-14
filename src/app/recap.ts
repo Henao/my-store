9e8e31614f1997bc54dadc69008d87fcbc8106bb
Julian <Henao>
2021-10-14 02:48:16 -0500

    Revisa sisntaxis de TypeScript

---
+++ src/app/recap.ts
@@ -0,0 +1,26 @@
+const username: string = 'Hola Angular';
+const var2: string | number = 1;
+
+const suma = (a: number, b: number) => {
+  return a + b;
+};
+
+suma(2, 3);
+
+// class Person {
+//   age: number;
+//   lastName: string;
+
+//   constructor(age: number, lastName: string) {
+//     this.age = age;
+//     this.lastName = lastName;
+//   }
+// }
+class Person{
+    constructor(public age: number, public lastName: string){
+        
+    }
+}
+
+const julian = new Person(33, 'Henao');
+julian.age;
